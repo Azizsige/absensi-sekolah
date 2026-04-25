76535f40c2ce281ec4ef4d04280768885a584465
azizsige <kurniawan00azizsige@gmail.com>
2026-04-25 22:42:55 +0700

    feat: integrate naive ui layout and setup dashboard skeleton

---
+++ resources/js/Components/Admin/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <n-page-header class="px-2 py-3 navbar relative z-100 border-b">
+        <template #title>
+            <div class="flex items-center">
+                <n-button mx-2 size="small" quaternary circle>
+                    <span class="font-bold">☰</span>
+                </n-button>
+                <div class="ml-4 font-semibold text-gray-700">
+                    Panel Admin Absensi
+                </div>
+            </div>
+        </template>
+
+        <template #extra>
+            <div class="flex items-center gap-4 pr-4">
+                <div class="user-info text-sm font-medium">Admin TU</div>
+                <n-button size="small" type="error" dashed>Logout</n-button>
+            </div>
+        </template>
+    </n-page-header>
+</template>
+
+<script setup>
+// Script dikosongin dulu karena logika interaktifnya kita handle nanti
+</script>
+
+<style lang="scss">
+.navbar {
+    border-bottom: solid 1px #e5e7eb;
+    padding-bottom: 0.8rem;
+    padding-top: 0.8rem;
+}
+</style>
---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <AdminLayout>
+        <Head title="Dashboard Admin" />
+
+        <div class="p-4">
+            <n-card
+                title="Selamat Datang, Senior Dev!"
+                size="large"
+                class="shadow-sm rounded-lg"
+            >
+                <p class="text-gray-600">
+                    Kalau lu bisa liat kotak ini dengan rapi di sebelah kanan
+                    Sidebar, berarti eksperimen Fase 1.5 kita SUKSES BESAR!
+                    Inertia.js dan Naive UI udah berhasil nyatu.
+                </p>
+
+                <div class="mt-4">
+                    <n-button type="primary">Tombol Tes Naive UI</n-button>
+                </div>
+            </n-card>
+        </div>
+    </AdminLayout>
+</template>
+
+<script setup>
+import { Head } from "@inertiajs/vue3";
+import AdminLayout from "../../Layouts/AdminLayout.vue";
+</script>
